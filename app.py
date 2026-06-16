3fb6c8454a80fc1dd7f5372eba6eb4f6e0308b6b
slow-burner <jyjs0727@gmail.com>
2026-06-16 22:35:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
-import pandas
+import pandas as pd
 
 st.title("파이썬 페이지 구축")
 
 st.write("파이썬 웹 사이트 성공!")
+
+uploaded_file = st.file_uploader()
+st.text_input("텍스트를 입력하세요")
+
+# df = pd.read_csv(r"C:\Users\party\OneDrive\바탕 화면\SQL\books.csv")
+# print(df.head())
+
